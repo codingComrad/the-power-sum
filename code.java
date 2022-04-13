492ad2a8942335f7829c197f2c8873bf0a9bd8af
codingComrad <103552265+codingComrad@users.noreply.github.com>
2022-04-13 09:25:31 +0530

    Update code.java

--- code.java
+++ code.java
@@ -1,16 +1,16 @@
     public  static int rec(int tar, int k, int idx){
         
-        if(tar ==0)return 1;
+        if(tar ==0)return 1; // base case
             
         int count =0;
         for(int i =idx+1; i<=tar;i++){
-            int pwr = (int)Math.pow(i,k);
-            if(tar-pwr>=0){
-                count+=rec(tar-pwr,k , i);
+            int pwr = (int)Math.pow(i,k);  
+            if(tar-pwr>=0){ // to restrict the unnecessary calls
+                count+=rec(tar-pwr,k , i); // recursive call
                 
-            }else break;
+            }else break;  //if any number leads to -ve target then we will break the loop as the numbers left will also di the same
         }
-        return count;
+        return count; 
     }
     
     public static int powerSum(int X, int N) {
